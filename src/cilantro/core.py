5051bb85dd5c7de2def08ace1da1e45babd65d52
iamgodot <xugodot@gmail.com>
2024-07-12 18:35:00 -0700

    feat: implement data structures for headers

--- src/cilantro/core.py
+++ src/cilantro/core.py
@@ -1,6 +1,85 @@
+from collections import defaultdict
+from collections.abc import Mapping
+from typing import Any, Iterator
+
+
 class Cilantro:
     def __init__(self, name: str):
         self.name = name
 
     def __call__(self):
         return "Hello world!"
+
+
+class Headers(Mapping):
+    def __init__(self, headers: list[tuple[bytes, bytes]]):
+        self._headers = headers
+        self._dict = defaultdict(list)
+        for b_key, b_value in headers:
+            key, value = b_key.decode().lower(), b_value.decode().lower()
+            self._dict[key].append(value)
+        # Duplicates could exist
+        for key, values in self._dict.items():
+            self._dict[key] = list(dict.fromkeys(values))
+
+    def __getitem__(self, key: str) -> list[str]:
+        if key not in self:
+            raise KeyError
+        return self._dict[key.lower()]
+
+    def __iter__(self) -> Iterator[str]:
+        return iter(self._dict)
+
+    def __len__(self) -> int:
+        return len(self._dict)
+
+    def __contains__(self, key: Any) -> bool:
+        return key.lower() in self._dict
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Headers):
+            return False
+        return self._dict == other._dict
+
+    def __str__(self) -> str:
+        return str(self.raw)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.raw})"
+
+    @property
+    def raw(self) -> list[tuple[bytes, bytes]]:
+        return self._headers
+
+    def get(self, key: str, default: Any = None) -> Any:
+        if key not in self:
+            return default
+        return self[key][0]
+
+    def list(self, key: str) -> list[str]:
+        if key not in self:
+            return []
+        return self[key]
+
+
+class MutableHeaders(Headers):
+    def __setitem__(self, key: str, value: str) -> None:
+        # TODO: validation
+        self._dict[key.lower()] = [value]
+
+    def __delitem__(self, key: str) -> None:
+        del self._dict[key.lower()]
+
+    def append(self, key: str, value: str) -> None:
+        if key not in self:
+            self[key] = value
+        elif value not in self[key]:
+            self._dict[key.lower()].append(value)
+
+    def set(self, key: str, value: str) -> None:
+        self[key] = value
+
+    def pop(self, key: str) -> list[str]:
+        values = self.list(key)
+        del self[key]
+        return values
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,98 @@
-from cilantro import Cilantro
+import pytest
+
+from cilantro import Cilantro, Headers, MutableHeaders
 
 
 def test_cilantro():
     cilantro = Cilantro("Cilantro")
     assert cilantro() == "Hello world!"
+
+
+def test_headers_internal():
+    raw = [
+        (b"Host", b"localhost"),
+        (b"Accept", b"text/html"),
+        (b"Accept", b"text/plain"),
+    ]
+    headers = Headers(raw)
+    headers_mutable = MutableHeaders(raw)
+    assert headers_mutable["host"] == headers_mutable["Host"] == ["localhost"]
+    assert (
+        headers_mutable["accept"]
+        == headers_mutable["Accept"]
+        == ["text/html", "text/plain"]
+    )
+
+    assert list(headers_mutable) == ["host", "accept"]
+    assert len(headers_mutable) == 2
+    assert "host" in headers_mutable and "accept" in headers_mutable
+
+    assert headers == headers_mutable
+
+    assert (
+        str(headers)
+        == str(headers_mutable)
+        == "[(b'Host', b'localhost'), (b'Accept', b'text/html'), (b'Accept', b'text/plain')]"  # noqa
+    )
+    assert (
+        repr(headers)
+        == "Headers([(b'Host', b'localhost'), (b'Accept', b'text/html'), (b'Accept', b'text/plain')])"  # noqa
+    )
+    assert (
+        repr(headers_mutable)
+        == "MutableHeaders([(b'Host', b'localhost'), (b'Accept', b'text/html'), (b'Accept', b'text/plain')])"  # noqa
+    )
+
+    headers_mutable["host"] = "127.0.0.1"
+    del headers_mutable["accept"]
+    assert headers_mutable["host"] == ["127.0.0.1"]
+    with pytest.raises(KeyError):
+        headers_mutable["accept"]
+
+
+def test_headers_read():
+    raw = [
+        (b"Host", b"localhost"),
+        (b"Accept", b"text/html"),
+        (b"Accept", b"text/plain"),
+    ]
+    headers = Headers(raw)
+    assert headers.raw == raw
+
+    assert headers.get("host") == headers.get("Host") == "localhost"
+    assert headers.get("accept") == headers.get("Accept") == "text/html"
+    assert headers.get("user-agent") is None
+    assert headers.get("user-agent", "text/html") == "text/html"
+
+    assert headers.list("host") == headers.list("Host") == ["localhost"]
+    assert (
+        headers.list("accept") == headers.list("Accept") == ["text/html", "text/plain"]
+    )
+    assert headers.list("user-agent") == []
+
+
+def test_headers_with_duplicate_input():
+    raw = [
+        (b"Host", b"localhost"),
+        (b"Accept", b"text/html"),
+        (b"Host", b"localhost"),
+    ]
+    headers = Headers(raw)
+    assert len(headers) == 2
+    assert headers.get("host") == "localhost"
+    assert headers.list("host") == ["localhost"]
+
+
+def test_headers_write():
+    headers = MutableHeaders([])
+    headers.append("accept", "text/html")
+    headers.append("accept", "text/plain")
+    assert headers.list("accept") == ["text/html", "text/plain"]
+
+    headers.set("accept", "application/json")
+    headers.set("host", "localhost")
+    assert headers.list("accept") == ["application/json"]
+    assert headers.list("host") == ["localhost"]
+
+    assert headers.pop("accept") == ["application/json"]
+    assert headers.get("accept") is None
